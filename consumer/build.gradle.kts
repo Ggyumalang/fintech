8c558191e80715b304383acc6277325d038d5fcf
Ggyumalang <khg2154@naver.com>
2023-06-16 21:53:44 +0900

    Feat : [feat-feat-Kafka_Consumer] Kafka - Consumer 부분 구축
    api 서버는 80 포트로 변경
    Consumer 서버의 포트는 8080으로 구동

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -2,4 +2,16 @@ plugins {}
 
 version = "0.0.1"
 
-dependencies {}
+dependencies {
+    //kafka
+    implementation(project(":kafka"))
+    implementation("org.springframework.kafka:spring-kafka:2.8.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
+
+    // Domain
+    implementation(project(":domain"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
